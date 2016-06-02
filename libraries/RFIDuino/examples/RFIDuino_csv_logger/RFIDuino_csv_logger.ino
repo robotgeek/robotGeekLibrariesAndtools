e277aafd074d873fdb1a05866c3144f47fa5a4f8
Kyle Granat <kyle@trossenrobotics.com>
2016-06-02 10:01:14 -0500

    Comment Fixes

--- libraries/RFIDuino/examples/RFIDuino_csv_logger/RFIDuino_csv_logger.ino
+++ libraries/RFIDuino/examples/RFIDuino_csv_logger/RFIDuino_csv_logger.ino
@@ -1,13 +1,12 @@
 /***************************************************************************
- * RFIDuino Hello World 
+ * RFIDuino CSV Logger
  *
  * This demo uses the RFIDuino sheild, a Geekduino (or other Arduino Board)
  * and the RFIDuino library to scan for a tag. Once a tag is read, the ID of
- * the tag is stored as an array of 5 byte-sized numbers. This array of numbers 
- * represents the ID. After a sucessful read, these 6 numbers will be sent over
- * the Arduino's Serial Port. This demo can also be used to send data to
- * XBee modules.
- *
+ * the tag's lower 4 bytes are stored into a single 'long' variable. This 4-byte
+ * tag ID is sent over the serial port to an application that will log and add a
+ * time stamp.
+ * 
  * The RFIDuino library is compatible with both versions of the RFIDuino shield
  * (1.1 and 1.2), but the user must initialize the library correctly. See 
  * 'HARDWARE VERSION' instructions near the beginning of the code.
@@ -18,6 +17,8 @@
  *
  *
  * Links
+ *    RFIDuino Logger Getting Started Guide
+ *      http://learn.robotgeek.com/projects/277-rfid-logger.html
  *    RFIDUino Getting Started Guide
  *      http://learn.robotgeek.com/getting-started/41-rfiduino/142-rfiduino-getting-started-guide.html
  *    RFIDUino Library Documentation
@@ -54,26 +55,8 @@ unsigned long tagID; //the lower 4 byted of the tag ID
 
 void setup()
 {
-  //begin serial communicatons at 9600 baud and print a startup message
+  //begin serial communicatons at 38400 baud and print a startup message
   SERIAL_PORT.begin(38400);
-//  SERIAL_PORT.print("RFID LOGGER");
-//  SERIAL_PORT.write(0x0A);//return character for next line 
-//  
-//  SERIAL_PORT.print("EEPROM LOGGING:");
-//  if(LOG_TO_EEPROM == 0)
-//  {
-//   SERIAL_PORT.print("Disabled:");
-//  SERIAL_PORT.write(0x0A);//return character for next line 
-//  }
-//  else
-//  {
-//   SERIAL_PORT.print("Enabled:");
-//  SERIAL_PORT.write(0x0A);//return character for next line 
-//  }
-//
-//  
-//  SERIAL_PORT.print("TagId");
-//  SERIAL_PORT.write(0x0A);//return character for next line 
 
   //The RFIDUINO hardware pins and user outputs(Buzzer / LEDS) are all initialized via pinMode() in the library initialization, so you don not need to to that manually
 }
@@ -83,19 +66,9 @@ void loop()
   //scan for a tag - if a tag is sucesfully scanned, return a 'true' and proceed
   if(myRFIDuino.scanForTag(tagData) == true)
   {
+    tagID = ((long)tagData[1] << 24) + ((long)tagData[2] << 16) + ((long)tagData[3] << 8) + tagData[4]; //generate a 4-byte number from the last 4 unique digits of th eID
 
-//    for(int n=0;n<5;n++)
-//    {
-//      SERIAL_PORT.print(tagData[n],DEC);  //print the byte in Decimal format
-//      if(n<4)//only print the comma on the first 4 nunbers
-//      {
-//        SERIAL_PORT.print(",");
-//      }
-//    }
-    tagID = ((long)tagData[1] << 24) + ((long)tagData[2] << 16) + ((long)tagData[3] << 8) + tagData[4]; //generate a 4-byte number from the last 4 unique digits
-
-    //generate leading zeros
-    //
+    //generate leading zeros for nice printing
     for(int i = 1; i < 9; i++)
     {
       if( tagID < pow(10,i) )
@@ -104,24 +77,17 @@ void loop()
       }
     }
 
-
-
-   
-      //  SERIAL_PORT.print(" ");
-    SERIAL_PORT.print(tagID); //print a header to the Serial port.  
-//    SERIAL_PORT.print(",@TIME"); //print a header to the Serial port.   
-      
+    SERIAL_PORT.print(tagID); //print the 4-byte tag ID
     SERIAL_PORT.write(0x0A);//return character for next line 
-    //SERIAL_PORT.print(",\n\r");//return character for next line
-             
+
+    //turn buzzer /led on quickly          
     digitalWrite(myRFIDuino.led2,HIGH);     //turn green LED on
     digitalWrite(myRFIDuino.buzzer, HIGH);   //turn the buzzer on
     delay(100);                             //wait for 1 second
     digitalWrite(myRFIDuino.buzzer, LOW);    //turn the buzzer off
     digitalWrite(myRFIDuino.led2,LOW);      //turn the green LED off
 
-    delay(1000);
-
+    delay(1000); //delay to prevent the tag from printing time-after time
   }
 
  }// end loop()
